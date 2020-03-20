bce9d4e2f45197c1d64c2678f7423b52611bc6a5
MaxBateman <mxdbateman@gmail.com>
2020-03-20 19:53:42 +0000

    remove trash

--- flaskblog/FlexiTerm-master/script/FlexiClustering.bat
+++
@@ -1,1 +0,0 @@
-java -Xmx1000M -cp .;.\lib\sqlite-jdbc-3.8.11.2.jar FlexiClustering > FlexiClustering.log
--- flaskblog/FlexiTerm-master/script/mixup_demo.bat
+++
@@ -1,4 +0,0 @@
-del text.labels
-
-java -Xmx1000M edu.cmu.minorthird.ui.RunMixup -labels text -mixup output.mixup -showResult -format minorthird -saveAs text.labels
-
