15032fdf0ae937fcf20bf9045bf889705aa239dd
c1737854 <batemanmd@cardiff.ac.uk>
2020-04-07 13:31:41 +0100

    toggle

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -8,24 +8,22 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 @main.route("/home")
-#@event.listens_for(db.session, "after_commit")
 def home():
     db.create_all()
     queriest = QueryT.query.filter_by(user_id=session.sid)
     queriest = queriest[::-1]
-    #tick_file = os.path.join(os.getcwd(), 'flaskblog/static/tick.png')
-    #if num matched > 0
-    #picture_file = "tickg.jpg"
+
     return render_template('home.html', queriest=queriest)
-
-
-#@event.listens_for(db.session, 'after_commit')
-#def receive_after_commit(session):
-#    print(696969669)
-#    return redirect(url_for('main.about'))
 
 
 @main.route("/about")
 def about():
     #run_shelly()
     return render_template('about.html', title='About')
+
+
+@main.route("/home/all")
+def home_all():
+    queriest = QueryT.query.all()
+    queriest = queriest[::-1]
+    return render_template('home.html', queriest=queriest, active1="", active2="active")
