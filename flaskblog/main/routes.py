775bd68da23eab0091a50f4c4149d1f4960f8c39
c1737854 <batemanmd@cardiff.ac.uk>
2020-04-08 16:01:52 +0100

    errors & pagination

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, redirect, url_for, session, request
+from flask import render_template, Blueprint, redirect, url_for, session, request, flash
 from flaskblog import db, sess
 from flaskblog.models import QueryT
 from sqlalchemy import event
