dc1bf9cef2e6ac727eeb13790e1ab3f79e864cd9
c1737854 <batemanmd@cardiff.ac.uk>
2020-04-17 17:01:44 +0100

    articalis

--- flaskblog/queries/forms.py
+++ flaskblog/queries/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, AlphaNumeric
+from wtforms.validators import DataRequired, Length
+from wtforms_validators import AlphaNumeric
 
 
 class QuerytForm(FlaskForm):
