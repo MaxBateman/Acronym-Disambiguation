6c178f3855069f35db303899d5c681a6abb554e7
c1737854 <batemanmd@cardiff.ac.uk>
2020-04-23 14:12:15 +0100

    email

--- flaskblog/queries/routes.py
+++ flaskblog/queries/routes.py
@@ -124,10 +124,9 @@ def queryt(queryt_id):
     form = Email()
     if form.is_submitted():
         if form.validate_on_submit():
-            msg = Message("ACRPUBMED - ", ADMINS[0], [form.email.data])
-            msg.body = "hell"
+            send_email("ACRPUBMED - ", ADMINS[0], [form.email.data],'hell')
             
-            send_em(msg)
+            #send_em(msg)
             flash('Email Sent!', 'success')
 
             return redirect(url_for('queries.queryt', queryt_id=queryt.id))
@@ -137,6 +136,11 @@ def queryt(queryt_id):
             return redirect(url_for('queries.queryt', queryt_id=queryt.id))
     return render_template('queryt.html', form=form, title=queryt.term, queryt=queryt, lfmatches=lfmatches, acrmatches=acrmatches, content=articles)
 
+def send_email(subject,sender,recipients,text_body):
+    print(subject,sender,recipients,text_body)
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body=text_body    
+    mail.send(msg)
 
 @rq.job
 def get_inp(data, potential_full, user_id, termdata=None):
