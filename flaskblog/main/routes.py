7bef7e9328362307cd6d3733c60bf454f2ce8aa7
c1737854 <batemanmd@cardiff.ac.uk>
2020-05-12 15:52:05 +0100

    1

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, redirect, url_for, session, request, flash
+from flask import render_template, Blueprint, redirect, url_for, session, request, flash, Markup
 from flaskblog import db, sess
 from flaskblog.models import QueryT
 from sqlalchemy import event
@@ -14,7 +14,7 @@ def home():
     try:
         queriest[0]
     except IndexError:
-        flash('Create a new query!', 'success')
+        flash(Markup('Create a <a href="{{ url_for('main.create_queryt') }}">new query!</a>', 'success'))
     page = request.args.get('page', 1, type=int)
     queriest = queriest.order_by(QueryT.date_posted.desc()).paginate(page=page, per_page=5)
 
